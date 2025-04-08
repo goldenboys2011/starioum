63a2e63fd539df5f8928c929510311b64e3ee174
goldenboys2011 <ody.chryssos@gmail.com>
2025-04-08 22:31:31 +0300

    idk body?

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { BoxReveal } from "@/components/magicui/box-reveal";
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 import { TypingAnimation } from "@/components/magicui/typing-animation";
 import { WordRotate } from "@/components/magicui/word-rotate";
 export default function Home() {
@@ -7,23 +8,45 @@ export default function Home() {
     <footer className="content-center justify-self-center mt-8">
       <TypingAnimation className="text-5xl font-mono">Starioum ✦</TypingAnimation>
     </footer>
-    <div className="content-center mt-30 ml-40" key="body">
-      <BoxReveal boxColor={"#232323"} duration={0.5}>
-          <p className="text-[3.5rem] font-semibold">
-            Who are we?
-          </p>
-        </BoxReveal>
-  
-        <BoxReveal boxColor={"#363636"} duration={0.5}>
-          <h2 className="mt-[.5rem] text-2xl flex items-center">
-            The Sience {" "} 
-            <WordRotate 
-              className="text-2xl font-bold text-black dark:text-white ml-1 underline" 
-              words={["Researchers", "Enginiars"]} 
-              duration={3500}
-            />
-          </h2>
-        </BoxReveal>
+    <div className="mt-30" key="body">
+      <div key="left" className="ml-40">
+        <BoxReveal boxColor={"#232323"} duration={0.5}>
+            <p className="text-[3.5rem] font-semibold">
+              Who are we?
+            </p>
+          </BoxReveal>
+    
+          <BoxReveal boxColor={"#363636"} duration={0.5}>
+            <h2 className="mt-[.5rem] text-2xl flex items-center">
+              The Sience {" "} 
+              <WordRotate 
+                className="text-2xl font-bold text-black dark:text-white ml-1 underline" 
+                words={["Researchers", "Engineers"]} 
+                duration={3500}
+              />
+            </h2>
+          </BoxReveal>
+        </div>    
+
+        <div key="right" className="justify-self-end mr-40 mt-10">
+          <BoxReveal boxColor={"#232323"} duration={0.5}>
+              <p className="text-[3.5rem] font-semibold">
+                What Do We Do?
+              </p>
+            </BoxReveal>
+      
+            <BoxReveal boxColor={"#363636"} duration={0.5}>
+              <h2 className="mt-[.5rem] text-2xl flex items-center">
+                <VelocityScroll
+                  className="max-w-120 text-2xl"
+                  numRows={1}
+                  defaultVelocity={-3}
+                  >
+                    Scientific Researches, Vallidations, News & More...
+                </VelocityScroll>
+              </h2>
+            </BoxReveal>
+        </div>
       </div>
     </>
   );
