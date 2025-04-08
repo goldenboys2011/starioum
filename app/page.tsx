62c3490fc787c89521c4561ac254110c4da95c7b
goldenboys2011 <ody.chryssos@gmail.com>
2025-04-08 17:55:52 +0300

    Created App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { TypingAnimation } from "@/components/magicui/typing-animation";
+import { Label } from "@radix-ui/react-label";
+
+export default function Home() {
+  return (
+    <>
+    <footer className="content-center justify-self-center mt-8">
+      <TypingAnimation className="text-5xl font-mono">Sotnhk</TypingAnimation>
+    </footer>
+    </>
+  );
+}
